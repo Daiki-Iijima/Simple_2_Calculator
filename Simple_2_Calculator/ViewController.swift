7b48f9d66657fd0bae75433f98f46835e209b303
Daiki Iijima <daiki.applecreate@gmail.com>
2020-02-14 01:23:53 +0900

    Initial Commit

---
+++ Simple_2_Calculator/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Simple_2_Calculator
+//
+//  Created by 飯島大樹 on 2020/02/14.
+//  Copyright © 2020 Daiki Iijima. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
